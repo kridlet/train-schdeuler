dd33ffce63304a7f297b3a1f973c07fd3e202785
dirk wiggins <dirk@echo19.com>
2018-03-22 23:35:00 -0400

    initial html and javascript

---
+++ assets/scripts/firebase-init.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+// Make sure to match the configuration to the script version number in the HTML
+// (Ex. 3.0 != 3.7.0)
+var config = {
+    apiKey: "AIzaSyCAyXQkifFrOuG46U5sTd4zzNfBJrMTk4E",
+    authDomain: "helloworld-265b2.firebaseapp.com",
+    databaseURL: "https://helloworld-265b2.firebaseio.com",
+    projectId: "helloworld-265b2",
+    storageBucket: "helloworld-265b2.appspot.com",
+    messagingSenderId: "95246432579"
+};
+
+firebase.initializeApp(config);
